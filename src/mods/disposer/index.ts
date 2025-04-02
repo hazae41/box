2153463d22ea16d733a49af31053d6560ec5532b
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-04-02 12:41:02 +0700

    d

---
+++ src/mods/disposer/index.ts
@@ -0,0 +1,61 @@
+import { AsyncDeferred, Deferred } from "mods/deferred/index.js"
+
+export class Disposer<T> implements Disposable {
+
+  constructor(
+    readonly value: T,
+    readonly clean: Deferred
+  ) { }
+
+  static with<T>(inner: T, clean: (inner: T) => void) {
+    return new Disposer(inner, new Deferred(() => clean(inner)))
+  }
+
+  static from<T>(disposable: T & Disposable) {
+    return new Disposer(disposable, new Deferred(() => disposable[Symbol.dispose]()))
+  }
+
+  [Symbol.dispose]() {
+    this.clean[Symbol.dispose]()
+  }
+
+  get() {
+    return this.value
+  }
+
+  async await<T>(this: Disposer<Promise<T>>) {
+    using _ = this.clean
+    return await this.get()
+  }
+
+}
+
+export class AsyncDisposer<T> implements AsyncDisposable {
+
+  constructor(
+    readonly value: T,
+    readonly clean: AsyncDeferred
+  ) { }
+
+  static with<T>(inner: T, clean: (inner: T) => Promise<void>) {
+    return new AsyncDisposer(inner, new AsyncDeferred(() => clean(inner)))
+  }
+
+  static from<T>(disposable: T & AsyncDisposable) {
+    return new AsyncDisposer(disposable, new AsyncDeferred(() => disposable[Symbol.asyncDispose]()))
+  }
+
+  async [Symbol.asyncDispose]() {
+    await this.clean[Symbol.asyncDispose]()
+  }
+
+  get() {
+    return this.value
+  }
+
+  async await<T>(this: AsyncDisposer<Promise<T>>) {
+    await using _ = this.clean
+    return await this.get()
+  }
+
+}
