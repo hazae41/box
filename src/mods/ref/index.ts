d7d6cb1ef26444a1ce05a545426876f67abc5254
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-05-25 13:42:31 +0700

    d

--- src/mods/ref/index.ts
+++ src/mods/ref/index.ts
@@ -4,10 +4,6 @@ export class Ref<T> {
     readonly value: T
   ) { }
 
-  static from<T>(value: T) {
-    return new Ref(value)
-  }
-
   [Symbol.dispose]() { }
 
   async [Symbol.asyncDispose]() { }
