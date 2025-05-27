307a87aaa55e0ea0ede5c7602468c8eb3c623f89
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-05-27 12:43:44 +0700

    d

--- src/mods/rc/index.ts
+++ src/mods/rc/index.ts
@@ -1,27 +1,43 @@
+import { Nullable } from "libs/nullable/index.js"
 import { Deferred } from "mods/deferred/index.js"
 import { Wrap } from "mods/wrap/index.js"
 
+export class RcError extends Error {
+  readonly #class = RcError
+  readonly name = this.#class.name
+
+  constructor() {
+    super(`Resource is already referenced`)
+  }
+}
+
 export class Rc<T> {
+
+  #count = 1
 
   constructor(
     readonly value: T,
-    readonly clean: Disposable,
-    readonly count: { value: number }
+    readonly clean: Disposable
   ) { }
 
   static wrap<T extends Disposable>(value: T) {
-    return new Rc(value, value, { value: 0 })
+    return new Rc(value, value)
   }
 
   static from<T>(value: Wrap<T>) {
-    return new Rc(value.get(), value, { value: 0 })
+    return new Rc(value.get(), value)
   }
 
   static with<T>(value: T, clean: (value: T) => void) {
-    return new Rc(value, new Deferred(() => clean(value)), { value: 0 })
+    return new Rc(value, new Deferred(() => clean(value)))
   }
 
   [Symbol.dispose]() {
+    this.#count--
+
+    if (this.#count > 1)
+      return
+
     this.clean[Symbol.dispose]()
   }
 
@@ -37,6 +53,39 @@ export class Rc<T> {
     this[Symbol.dispose]()
 
     return this.value
+  }
+
+  getOrNull(): Nullable<T> {
+    if (this.#count > 1)
+      return
+
+    return this.value
+  }
+
+  getOrThrow(): T {
+    if (this.#count > 1)
+      throw new RcError()
+
+    return this.value
+  }
+
+  checkOrNull(): Nullable<this> {
+    if (this.#count > 1)
+      return
+
+    return this
+  }
+
+  checkOrThrow(): this {
+    if (this.#count > 1)
+      throw new RcError()
+
+    return this
+  }
+
+  clone() {
+    this.#count++
+    return this
   }
 
 }
